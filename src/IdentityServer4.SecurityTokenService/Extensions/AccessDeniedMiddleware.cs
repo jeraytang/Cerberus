86e4b9a32b35ef5d60d489f84917b611af5a98ad
zhengping.tang <zhengping.tang@pamirs.com>
2020-04-15 20:00:47 +0800

    '打上特性AccessDeniedAttribute禁止访问，直接跳转主页'

---
+++ src/IdentityServer4.SecurityTokenService/Extensions/AccessDeniedMiddleware.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Mvc.Controllers;
+
+namespace IdentityServer4.SecurityTokenService.Extensions
+{
+    public class AccessDeniedMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public AccessDeniedMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var endpoint = context.Features.Get<IEndpointFeature>()?.Endpoint;
+            var accessDeniedAttribute = endpoint?.Metadata.GetMetadata<AccessDeniedAttribute>();
+            if (accessDeniedAttribute != null)
+            {
+                //标记禁止访问的特性-跳转主页
+                context.Response.StatusCode = 403;
+                context.Response.Redirect("/");
+            }
+
+            await _next(context);
+        }
+    }
+}
---
+++ src/IdentityServer4.SecurityTokenService/Extensions/AccessDeniedMiddlewareExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+namespace IdentityServer4.SecurityTokenService.Extensions
+{
+    public static class AccessDeniedMiddlewareExtension
+    {
+        /// <summary>
+        /// 打上AccessDeniedAttribute特性，则跳转回主页
+        /// </summary>
+        /// <param name="app"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static IApplicationBuilder UseAccessDenied(this IApplicationBuilder app)
+        {
+            if (app == null)
+            {
+                throw new ArgumentNullException(nameof(app));
+            }
+
+            return app.UseMiddleware<AccessDeniedMiddleware>();
+        }
+    }
+}
