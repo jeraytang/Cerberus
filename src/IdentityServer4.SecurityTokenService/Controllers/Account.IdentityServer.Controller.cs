8b558b98b8c771ba09fd512878e7bdf3e0405ca0
zhengping.tang <zhengping.tang@pamirs.com>
2020-04-20 13:20:06 +0800

    '登录错误信息提示修改'

--- src/IdentityServer4.SecurityTokenService/Controllers/Account.IdentityServer.Controller.cs
+++ src/IdentityServer4.SecurityTokenService/Controllers/Account.IdentityServer.Controller.cs
@@ -145,7 +145,7 @@ namespace IdentityServer4.SecurityTokenService.Controllers
                 {
                     await _events.RaiseAsync(new UserLoginFailureEvent(model.Username, "invalid credentials",
                         clientId: context?.ClientId));
-                    ModelState.AddModelError(nameof(model.Password), "密码git错误！");
+                    ModelState.AddModelError(nameof(model.Password), "密码错误！");
                 }
             }
 
