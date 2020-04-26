0512f59e2e97a850d892940a9093362c1dc5c41c
zhengping.tang <zhengping.tang@pamirs.com>
2020-04-26 14:45:31 +0800

    AccountCorsOrigins

--- src/IdentityServer4.SecurityTokenService/Startup.cs
+++ src/IdentityServer4.SecurityTokenService/Startup.cs
@@ -30,6 +30,9 @@ namespace IdentityServer4.SecurityTokenService
 
         public IConfiguration Configuration { get; }
 
+        private const string AccountAllowOriginsPolicy = "AccountAllowOrigins";
+
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
@@ -102,6 +105,22 @@ namespace IdentityServer4.SecurityTokenService
             services.AddTransient<TokenEndpoint>();
             services.AddSingleton(new Hosting.Endpoint("ExternalToken", "/connect/external-token",
                 typeof(TokenEndpoint)));
+
+            //AddPolicy allow special origins
+            var origins = Configuration.GetSection("AccountCorsOrigins").Get<string[]>();
+            if (origins != null && origins.Length > 0)
+            {
+                services.AddCors(opt =>
+                {
+                    opt.AddPolicy(AccountAllowOriginsPolicy,
+                        config =>
+                        {
+                            config.WithOrigins(origins)
+                                .AllowAnyHeader()
+                                .AllowAnyMethod();
+                        });
+                });
+            }
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -128,15 +147,17 @@ namespace IdentityServer4.SecurityTokenService
             app.UseStaticFiles();
 
             app.UseRouting();
-            
-            var accessDeniedPages= Configuration.GetSection("AccessDeniedPages").Get<List<AccessDeniedPage>>();
+
+            app.UseCors(AccountAllowOriginsPolicy);
+
+            var accessDeniedPages = Configuration.GetSection("AccessDeniedPages").Get<List<AccessDeniedPage>>();
             app.UseAccessDenied(accessDeniedPages);
-            
+
             app.UseMySqlIdentityServer();
             app.MigrateIdentity();
-            
+
             app.UseAuthorization();
-            
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
